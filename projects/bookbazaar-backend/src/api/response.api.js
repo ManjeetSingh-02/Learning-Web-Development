bd052a23d697f80992820f263ee5810c701fe876
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-08-15 00:51:24 +0530

    projects moved to their own repo's

--- projects/bookbazaar-backend/src/api/response.api.js
+++
@@ -1,9 +0,0 @@
-// class to standardize API responses
-export class APIResponse {
-  constructor(statusCode, message = 'Success', data = []) {
-    this.success = true;
-    this.statusCode = statusCode;
-    this.data = data;
-    this.message = message;
-  }
-}
--- projects/bookbazaar-backend/src/api/v1/carts/carts.zodschemas.js
+++
@@ -1,7 +0,0 @@
-// import external modules
-import { z } from 'zod';
-
-// zod schema for updateCart
-export const updateCartSchema = z.object({
-  bookId: z.string().nonempty('Book ID is required'),
-});
